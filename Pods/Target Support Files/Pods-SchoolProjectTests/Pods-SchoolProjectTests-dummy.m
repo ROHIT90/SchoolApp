2bce8c12a11d2b696dbaf01706e24049f43d2c04
Rohit <1990.rohitnarwal@gmail.com>
2017-05-01 00:04:01 -0400

    Added firebase pod

---
+++ Pods/Target Support Files/Pods-SchoolProjectTests/Pods-SchoolProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SchoolProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_SchoolProjectTests
+@end
---
+++ Pods/Target Support Files/Pods-SchoolProjectUITests/Pods-SchoolProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SchoolProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SchoolProjectUITests
+@end
