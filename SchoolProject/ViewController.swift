14b0dae0b5a033b01f78db82ee0b527d438d0291
Rohit <1990.rohitnarwal@gmail.com>
2017-04-30 23:38:51 -0400

    Initial commit

--- SchoolProject/ViewController.swift
+++ SchoolProject/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  SchoolProject
-//
-//  Created by Fnu, Rohit on 4/30/17.
-//  Copyright © 2017 Fnu, Rohit. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
