06241d625b277305e2284fcf1a8300ec25fecf45
Rohit <1990.rohitnarwal@gmail.com>
2017-04-30 23:35:30 -0400

    Initial Commit

---
+++ SchoolProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchoolProject
+//
+//  Created by Fnu, Rohit on 4/30/17.
+//  Copyright © 2017 Fnu, Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
