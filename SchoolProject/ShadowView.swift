428fde47ee37926956533ddc253fccd4b13d2a3e
Rohit <1990.rohitnarwal@gmail.com>
2017-05-16 22:42:22 -0400

    WIP

--- SchoolProject/ShadowView.swift
+++ SchoolProject/ShadowView.swift
@@ -16,5 +16,6 @@ class ShadowView: UIView {
         layer.shadowOpacity = 0.0
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width:1.0, height:1.0)
+        layer.cornerRadius = 2.0
     }
 }
