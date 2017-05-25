9d75f413002800bae398cfc4ff61194d6199bb56
Rohit <1990.rohitnarwal@gmail.com>
2017-05-25 00:14:21 -0400

    Finished adding usename in db

--- SchoolProject/AppDelegate.swift
+++ SchoolProject/AppDelegate.swift
@@ -1,47 +1,24 @@
 import UIKit
 import Firebase
-
 import FBSDKCoreKit
 import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
-        
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-
         return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         FBSDKAppEvents.activateApp()
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-
-    }
-
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         let handle = FBSDKApplicationDelegate.sharedInstance().application(application, open: url as URL!, sourceApplication: sourceApplication, annotation: annotation)
         return handle
     }
-
 }
 
