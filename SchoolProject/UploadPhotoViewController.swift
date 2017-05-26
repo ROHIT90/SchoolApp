f451563ca344d26b78f632ca90ad9f46b9b9b228
Rohit <1990.rohitnarwal@gmail.com>
2017-05-26 11:04:25 -0400

    Added image picker

--- SchoolProject/UploadPhotoViewController.swift
+++ SchoolProject/UploadPhotoViewController.swift
@@ -5,11 +5,18 @@ import SwiftKeychainWrapper
 class UploadPhotoViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addImageView: UIImageView!
     let photoView = UploadPhotoView()
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
                 for snap in snapshot {
@@ -25,16 +32,16 @@ class UploadPhotoViewController: UIViewController,UIImagePickerControllerDelegat
         
     }
     
-    @IBAction func photoButtonTapped(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-        }
+    @IBAction func photoGestureTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            addImageView.image = image
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
     @IBAction func logOutButtonTapped(_ sender: Any) {
         try! FIRAuth.auth()!.signOut()
         KeychainWrapper.standard.removeObject(forKey: KEY_UID)
@@ -46,7 +53,6 @@ extension UploadPhotoViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 400.0
     }
-    
 }
 
 extension UploadPhotoViewController: UITableViewDataSource {
@@ -62,7 +68,5 @@ extension UploadPhotoViewController: UITableViewDataSource {
             cell.configureCell(post: post)
             return cell
         } else { return PhotoTableViewCell() }
-        
     }
-    
 }
